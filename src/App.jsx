98aa73cfddfaba5d41bd4c2bcc60033d5cb16de6
Stefan Hancar <stefan@hancar.sk>
2021-05-13 10:20:10 +0200

    Configure prettier; Add lint-stagged

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Home } from "./pages/Home";
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Home } from './pages/Home'
 
 export const App = () => (
   <ChakraProvider>
     <Home />
   </ChakraProvider>
-);
+)
